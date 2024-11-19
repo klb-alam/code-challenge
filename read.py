f702a510c8a5c497a22613239c60b62f5fae04ae
aspoofer0224 <alexlam@kasagilabo.com>
2024-11-19 17:31:55 +0800

    update read.py

--- read.py
+++ read.py
@@ -11,24 +11,7 @@ def process_file(filename):
     with open(filename, 'r') as f:
         content = f.read()
         objects = content.strip(',').split(',')  
-
-        for obj in objects:
-            try:
-                value = int(obj)
-                print(f"Object: {value}, Type: Integer")
-            except ValueError:
-                try:
-                    value = float(obj)
-                    print(f"Object: {value}, Type: Real Number")
-                except ValueError:
-                    if obj.isalnum():
-                        print(f"Object: {obj}, Type: Alphanumeric")
-                    else:
-                        stripped_obj = obj.strip()
-                        if stripped_obj.isalnum():
-                            print(f"Object: {stripped_obj}, Type: Alphanumeric")
-                        else:
-                            print(f"Object: {obj}, Type: Alphabetical String")
+        print(objects)
 
 if __name__ == "__main__":
     filename = "random.txt"
