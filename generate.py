ac5dd4ece07a677a720d8832ae79fc9968b9beec
klb-alam <alexlam@kasagilabo.com>
2024-11-21 14:09:18 +0800

    type hint the lambda function

--- generate.py
+++ generate.py
@@ -1,6 +1,7 @@
 import random
 import string
 import os
+from typing import Callable
 
 
 '''
@@ -11,9 +12,10 @@ each object will be separated by a ",". These are the 4 objects: alphabetical st
 The alphanumerics should contain a random number of spaces before and after it (not exceeding 10 spaces). The output should be 10MB in size.
 '''
 
-generate_random_string = lambda length: ''.join(random.choice(string.ascii_letters) for _ in range(length))
-generate_random_real = lambda: random.uniform(-10, 10)
-generate_random_integer = lambda: random.randint(-100, 100)
+
+generate_random_string: Callable[[int], str] = lambda length: ''.join(random.choice(string.ascii_letters) for _ in range(length))
+generate_random_real: Callable[[], float] = lambda: random.uniform(-10, 10)
+generate_random_integer: Callable[[], int] = lambda: random.randint(-100, 100)
 
 
 def generate_random_alphanumeric():
